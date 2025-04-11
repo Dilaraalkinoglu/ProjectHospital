ad9df40078ee1cd7c25226ac475ce14132c7ca95
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-12 00:32:44 +0300

    Hastane Sistemi uygulamamızın girişler kısmı oluşturuldu.

---
+++ FrmGirisler.cs
@@ -0,0 +1,31 @@
+namespace Proje_Hastane
+{
+    public partial class FrmGirisler : Form
+    {
+        public FrmGirisler()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHastaGiris_Click(object sender, EventArgs e)
+        {
+            FrmHastaGiris fr = new FrmHastaGiris();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void btnDoktorGiris_Click(object sender, EventArgs e)
+        {
+            FrmDoktorGiris fr = new FrmDoktorGiris();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void btnSekreterGiris_Click(object sender, EventArgs e)
+        {
+            FrmSekreterGiris fr = new FrmSekreterGiris();
+            fr.Show();
+            this.Hide();
+        }
+    }
+}
