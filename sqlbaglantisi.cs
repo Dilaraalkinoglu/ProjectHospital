4047b7cf0dc0ffce3e926d8d073f6d8e8495f31f
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-12 00:38:45 +0300

    Hastane sistemine kayıtlı kişilerin verilerini tuttuğumuz veritabanına erişim sağlanması için gerekli sınıf oluşturuldu.

---
+++ sqlbaglantisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+
+namespace Proje_Hastane
+{
+    class SqlBaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-E8R1RBK\\SQLEXPRESS;Initial Catalog=HastaneProje;Integrated Security=True;TrustServerCertificate=True"); // Add a semicolon here
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
