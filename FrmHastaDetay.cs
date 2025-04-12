ccd287c52e59e82bd654843d19373822e5fe8f56
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-12 17:16:43 +0300

    Hastanın ad soyad bilgileri görünür, randevu geçmişini görüntüleyebilir ve branşlara ulaşabilir.

--- FrmHastaDetay.cs
+++ FrmHastaDetay.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
@@ -15,6 +16,42 @@ namespace Proje_Hastane
         public FrmHastaDetay()
         {
             InitializeComponent();
+        }
+
+        public string tc;
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        private void FrmHastaDetay_Load(object sender, EventArgs e)
+        {
+            lblTC.Text = tc;
+
+
+            //Ad Soyad bilgilerini çekme
+            SqlCommand komut = new SqlCommand("Select HastaAd, HastaSoyad From Table_Hastalar where HastaTC=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", lblTC.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                lblAdSoyad.Text = dr[0] + " " + dr[1];
+            }
+            bgl.baglanti().Close();
+
+            //Randevu geçmişini listeleme
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Randevular where HastaTC=" + tc, bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            // Branşları Çekme
+
+            SqlCommand komut2 = new SqlCommand("Select BransAd From Table_Branslar", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                cmbBrans.Items.Add(dr2[0]);
+            }
+            bgl.baglanti().Close();
+
         }
     }
 }
