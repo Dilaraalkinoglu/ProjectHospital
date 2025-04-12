a5bf78555d4157cc3ff472d1240c0f460e8ff9c3
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-13 02:01:45 +0300

    Sekreterler TC kimlik no ve şifre ile sisteme giriş yapabilirler. q

--- FrmSekreterGiris.cs
+++ FrmSekreterGiris.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
-    public partial class FrmSekreterGiris: Form
+    public partial class FrmSekreterGiris : Form
     {
         public FrmSekreterGiris()
         {
             InitializeComponent();
+        }
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("Select * From Table_Sekreter where SekreterTC=@p1 and SekreterSifre=@p2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", MskTC.Text);
+            komut.Parameters.AddWithValue("@p2", txtSifre.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                FrmSekreterDetay frs = new FrmSekreterDetay();
+                frs.TCnumara = MskTC.Text; // TC numarasını detay formuna gönderiyoruz.
+                frs.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı TC veya Şifre");
+            }
+            bgl.baglanti().Close();
+
         }
     }
 }
