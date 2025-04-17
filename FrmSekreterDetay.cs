c0e65a48af67171aeedec40608f1528b8fc3ee81
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-18 00:03:32 +0300

    Randevu oluşturma, branş ve doktorları görüntüleme özellikleri eklendi.

--- FrmSekreterDetay.cs
+++ FrmSekreterDetay.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
@@ -20,6 +21,66 @@ namespace Proje_Hastane
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+        public string TCnumara;
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        private void FrmSekreterDetay_Load(object sender, EventArgs e)
+        {
+            lblTC.Text = TCnumara;
+            // Ad Soyad bilgilerini çekme 
+            SqlCommand komut1 = new SqlCommand("Select SekreterAdSoyad From Table_Sekreter where SekreterTC=@p1", bgl.baglanti());
+            komut1.Parameters.AddWithValue("@p1", lblTC.Text);
+            SqlDataReader dr1 = komut1.ExecuteReader();
+            while (dr1.Read())
+            {
+                lblAdSoyad.Text = dr1[0].ToString();
+            }
+            bgl.baglanti().Close();
+
+            // Branşları listeleme
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Branslar", bgl.baglanti());
+            da.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+
+            //Doktorları listeye aktarma 
+            DataTable dt2 = new DataTable();
+            SqlDataAdapter da2 = new SqlDataAdapter("Select (DoktorAd + ' ' + DoktorSoyad) as 'Doktorlar',DoktorBrans From Table_Doktorlar", bgl.baglanti());
+            da2.Fill(dt2);
+            dataGridView2.DataSource = dt2;
+
+            //Branşı combobox'a aktarma 
+            SqlCommand komut2 = new SqlCommand("Select BransAd From Table_Branslar", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                cmbBrans.Items.Add(dr2[0]);
+            }
+            bgl.baglanti().Close();
+        }
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand komutkaydet = new SqlCommand("insert into Table_Randevular (RandevuTarih,RandevuSaat,RandevuBrans,RandevuDoktor) values (@r1,@r2,@r3,@r4)", bgl.baglanti());
+            komutkaydet.Parameters.AddWithValue("@r1", mskTarih.Text);
+            komutkaydet.Parameters.AddWithValue("@r2", mskSaat.Text);
+            komutkaydet.Parameters.AddWithValue("@r3", cmbBrans.Text);
+            komutkaydet.Parameters.AddWithValue("@r1", cmbDoktor.Text);
+            komutkaydet.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Randevu oluşturuldu.");
+        }
+
+        private void cmbBrans_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbDoktor.Items.Clear();
+            SqlCommand komut = new SqlCommand("Select DoktorAd, DoktorSoyad From Table_Doktorlar where DoktorBrans=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", cmbBrans.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbDoktor.Items.Add(dr[0] + " " + dr[1]);
+            }
         }
     }
 }
