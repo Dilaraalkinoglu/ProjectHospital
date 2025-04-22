8690f765f24f1a1a1fe9b249dd6e6936ece12e9c
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-22 16:52:21 +0300

    Randevu listesi tamamlandı.

--- FrmRandevuListesi.cs
+++ FrmRandevuListesi.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
-    public partial class FrmRandevuListesi: Form
+    public partial class FrmRandevuListesi : Form
     {
         public FrmRandevuListesi()
         {
             InitializeComponent();
+        }
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        private void FrmRandevuListesi_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Randevular", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
         }
     }
 }
