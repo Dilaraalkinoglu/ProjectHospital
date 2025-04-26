bf571daaa3c94ea75f7195b5af5d1a54a929306d
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-26 16:15:14 +0300

    Duyurular paneli oluşturuldu.

--- FrmDuyurular.cs
+++ FrmDuyurular.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
-    public partial class FrmDuyurular: Form
+    public partial class FrmDuyurular : Form
     {
         public FrmDuyurular()
         {
             InitializeComponent();
+        }
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        private void FrmDuyurular_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Duyurular", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
     }
 }
