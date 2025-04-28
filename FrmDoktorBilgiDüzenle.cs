6590ae47f2dc7d9fa3a003c2dd94d6cf110c3531
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-28 13:33:54 +0300

    Doktor bilgi güncelleme paneli tamamlanmıştır.

--- FrmDoktorBilgiDüzenle.cs
+++ FrmDoktorBilgiDüzenle.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
-    public partial class FrmDoktorBilgiDüzenle: Form
+    public partial class FrmDoktorBilgiDüzenle : Form
     {
         public FrmDoktorBilgiDüzenle()
         {
             InitializeComponent();
+        }
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        public string TCNO;
+
+        private void FrmDoktorBilgiDüzenle_Load(object sender, EventArgs e)
+        {
+            mskTC.Text = TCNO;
+            SqlCommand komut = new SqlCommand("Select * From Table_Doktorlar where DoktorTC=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", mskTC.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                txtAd.Text = dr[1].ToString();
+                txtSoyad.Text = dr[2].ToString();
+                cmbBrans.Text = dr[3].ToString();
+                txtSifre.Text = dr[5].ToString();
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("Update Table_Doktorlar set DoktorAd=@p1, DoktorSoyad=@p2, DoktorBrans=@p3, DoktorSifre=@p4 where DoktorTC=@p5", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", txtAd.Text);
+            komut.Parameters.AddWithValue("@p2", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", cmbBrans.Text);
+            komut.Parameters.AddWithValue("@p4", txtSifre.Text);
+            komut.Parameters.AddWithValue("@p5", mskTC.Text);
+            komut.ExecuteNonQuery();
+            MessageBox.Show("Bilgileriniz güncellenmiştir.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            bgl.baglanti().Close();
         }
     }
 }
