9600490fd875528e742d0cb483b8a30344ff64d2
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-28 13:09:04 +0300

    Doktor giriş paneli tamamlandı.

--- FrmDoktorGiris.cs
+++ FrmDoktorGiris.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
-    public partial class FrmDoktorGiris: Form
+    public partial class FrmDoktorGiris : Form
     {
         public FrmDoktorGiris()
         {
             InitializeComponent();
+        }
+
+        SqlBaglantisi bgl = new SqlBaglantisi();
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("Select * From Table_Doktorlar where DoktorTC=@p1 and DoktorSifre=@p2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", MskTC.Text);
+            komut.Parameters.AddWithValue("@p2", txtSifre.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                FrmDoktorDetay frmDoktorDetay = new FrmDoktorDetay();
+                frmDoktorDetay.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı TC veya Şifre", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            bgl.baglanti().Close();
         }
     }
 }
