b88b73147cd7cff87d95592878aa18adb080aa99
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-28 13:00:05 +0300

    Hasta detay paneli tamamlanmıştır.

--- FrmHastaDetay.cs
+++ FrmHastaDetay.cs
@@ -53,5 +53,51 @@ namespace Proje_Hastane
             bgl.baglanti().Close();
 
         }
+
+        private void cmbBrans_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbDoktor.Items.Clear();
+
+            SqlCommand komut3 = new SqlCommand("Select DoktorAd, DoktorSoyad From Table_Doktorlar where DoktorBrans=@p1", bgl.baglanti());
+            komut3.Parameters.AddWithValue("@p1", cmbBrans.Text);
+            SqlDataReader dr3 = komut3.ExecuteReader();
+            while (dr3.Read())
+            {
+                cmbDoktor.Items.Add(dr3[0] + " " + dr3[1]);
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void cmbDoktor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Randevular where RandevuBrans='" + cmbBrans.Text + "'" + "and RandevuDoktor='" + cmbDoktor.Text + "'" + "and RandevuDurum=0", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+        }
+
+        private void lnkBilgiDuzenle_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmBilgiDuzenle fr = new FrmBilgiDuzenle();
+            fr.TCno = lblTC.Text;
+            fr.Show();
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView2.SelectedCells[0].RowIndex;
+            txtID.Text = dataGridView2.Rows[secilen].Cells[0].Value.ToString();
+        }
+
+        private void btnRandevuAl_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("update Table_Randevular set RandevuDurum=1, HastaTc=@p1, HastaSikayet=@p2 where Randevuid=@p3", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", lblTC.Text);
+            komut.Parameters.AddWithValue("@p2", rchSikayet.Text);
+            komut.Parameters.AddWithValue("@p3", txtID.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Randevunuz alınmıştır.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
     }
 }
