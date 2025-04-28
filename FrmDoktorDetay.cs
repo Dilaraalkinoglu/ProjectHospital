d5a21bd928dec9fad83662291797ef70977730cd
Dilaraalkinoglu <dilaraalkinoglu80@gmail.com>
2025-04-28 13:40:47 +0300

    Doktor detay paneli tamamlanmıştır.

--- FrmDoktorDetay.cs
+++ FrmDoktorDetay.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Proje_Hastane
 {
@@ -17,9 +18,53 @@ namespace Proje_Hastane
             InitializeComponent();
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        SqlBaglantisi bgl = new SqlBaglantisi();
+        public string TC;
+        private void FrmDoktorDetay_Load(object sender, EventArgs e)
         {
+            lblTC.Text = TC;
 
+            // Ad Soyad bilgilerini çekme 
+            SqlCommand komut = new SqlCommand("Select DoktorAd, DoktorSoyad From Table_Doktorlar where DoktorTC=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", lblTC.Text);
+            komut.ExecuteNonQuery();
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                lblAdSoyad.Text = dr[0] + " " + dr[1];
+            }
+            bgl.baglanti().Close();
+
+            // Randevular 
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Table_Randevular where RandevuDoktor='" + lblAdSoyad.Text + "'", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            bgl.baglanti().Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            FrmDoktorBilgiDüzenle frmDoktorBilgiDüzenle = new FrmDoktorBilgiDüzenle();
+            frmDoktorBilgiDüzenle.TCNO = lblTC.Text;
+            frmDoktorBilgiDüzenle.Show();
+        }
+
+        private void btnDuyurular_Click(object sender, EventArgs e)
+        {
+            FrmDuyurular fr = new FrmDuyurular();
+            fr.Show();
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            rchSikayet.Text = dataGridView1.Rows[secilen].Cells[7].Value.ToString();
         }
     }
 }
